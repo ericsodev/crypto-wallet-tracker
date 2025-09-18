5f348e4e6f015b5d605ea1739136464a358eeb04
Eric <eric.so.dev@gmail.com>
2025-09-18 10:22:40 -0400

    feat: create trx table

---
+++ database/migrations/1757619599251_create_transaction_table.ts
@@ -0,0 +1,24 @@
+import { sql, type Kysely } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('transaction')
+    .addColumn('createdAt', 'timestamptz', col => col.notNull().defaultTo(sql`current_timestamp`))
+    .addColumn('updatedAt', 'timestamptz', col => col.notNull().defaultTo(sql`current_timestamp`))
+    .addColumn('deletedAt', 'timestamptz')
+    .addColumn('userId', 'uuid', col => col.notNull().references('user.id').onDelete('cascade'))
+    .addColumn('walletId', 'uuid', col => col.notNull().references('wallet.id').onDelete('cascade'))
+    .addColumn('type', 'varchar(31)', col => col.notNull())
+    .addColumn('timestamp', 'timestamptz', col => col.notNull())
+    .addColumn('blockNumber', 'integer', col => col.notNull())
+    .addColumn('hash', 'varchar', col => col.notNull())
+    .addColumn('amount', 'varchar', col => col.notNull())
+    .addColumn('fee', 'varchar', col => col.notNull())
+    .addColumn('senderAddress', 'varchar', col => col.notNull())
+    .addColumn('recepientAddress', 'varchar', col => col.notNull())
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('transaction').execute();
+}
---
+++ packages/common/src/database/repositories/transaction-repository.ts
@@ -0,0 +1,25 @@
+import type { Insertable, Kysely } from 'kysely';
+import { createManyRows, createRow, getRow, getRows } from './utils/kysely-crud-functions';
+import type { Database } from '../database';
+import type { Selectable } from 'kysely';
+
+export type TransactionDTO = Selectable<Database['transaction']>;
+
+export class TransactionRepository {
+  constructor(private readonly db: Kysely<Database>) {}
+  async get(id: string): Promise<TransactionDTO | undefined> {
+    return getRow(this.db, 'transaction', { id }).executeTakeFirst();
+  }
+
+  async getByFilter(filters: Partial<TransactionDTO>): Promise<TransactionDTO[]> {
+    return getRows(this.db, 'transaction', filters).execute();
+  }
+
+  async createTransaction(transaction: Insertable<Database['transaction']>): Promise<TransactionDTO> {
+    return createRow(this.db, 'transaction', transaction);
+  }
+
+  async createManyTransactions(transaction: Insertable<Database['transaction']>[]): Promise<TransactionDTO[]> {
+    return createManyRows(this.db, 'transaction', transaction);
+  }
+}
